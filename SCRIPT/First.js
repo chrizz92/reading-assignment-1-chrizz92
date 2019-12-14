bc171b869b72162b3b9c9f75b97596a840927002
Christian Schadler <c.schadler@HTBLALeonding.onmicrosoft.com>
2019-12-14 12:51:21 +0100

    Seite-1

--- SCRIPT/First.js
+++ SCRIPT/First.js
@@ -1,3 +1,20 @@
 /**
  * 
  */
+
+function Sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+
+async function test() {
+    console.log("Vor der sleep-Funktion");
+    await Sleep(5000); // Pausiert die Funktion für 3 Sekunden
+    console.log("Nach der Sleep Funktion");
+}
+
+test();
+
+
+
+
